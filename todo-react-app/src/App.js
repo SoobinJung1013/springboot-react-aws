0ba1dc3e279eeb6c9ff2aabc71d2ee8a7093c83a
Judy <judyb0626@gmail.com>
2022-03-14 01:08:20 -0700

    feat: connect front-end and back-end

--- todo-react-app/src/App.js
+++ todo-react-app/src/App.js
@@ -4,6 +4,7 @@ import "./App.css";
 import Todo from "./Todo.js";
 import AddTodo from "./AddTodo.js";
 import { Paper, List, Container } from "@material-ui/core";
+import { call } from "./service/ApiService.js";
 class App extends React.Component {
   constructor(props) {
     super(props);
@@ -11,31 +12,41 @@ class App extends React.Component {
       items: [],
     };
   }
+  componentDidMount() {
+    call("/todo", "GET", null).then((response) =>
+      this.setState({ items: response.data })
+    );
+  }
 
   add = (item) => {
-    const thisItems = this.state.items;
-    item.id = "ID-" + thisItems.length;
-    item.done = false;
-    thisItems.push(item);
-    this.setState({ items: thisItems });
-    console.log("items : ", this.state.items);
+    call("/todo", "POST", item).then((response) =>
+      this.setState({ items: response.data })
+    );
   };
 
   delete = (item) => {
-    const thisItems = this.state.items;
-    console.log("Before Update Items : ", this.state.items);
-    const newItems = thisItems.filter((e) => e.id !== item.id);
-    this.setState({ items: newItems }, () => {
-      // 디버깅 콜백
-      console.log("Update Items : ", this.state.items);
-    });
+    call("/todo", "DELETE", item).then((response) =>
+      this.setState({ items: response.data })
+    );
   };
+
+  update = (item) => {
+    call("/todo", "PUT", item).then((response) =>
+      this.setState({ items: response.data })
+    );
+  };
+
   render() {
     var todoItems = this.state.items.length > 0 && (
       <Paper style={{ margin: 16 }}>
         <List>
           {this.state.items.map((item, idx) => (
-            <Todo item={item} key={item.id} delete={this.delete} />
+            <Todo
+              item={item}
+              key={item.id}
+              delete={this.delete}
+              update={this.update}
+            />
           ))}
         </List>
       </Paper>
--- todo-react-app/src/Todo.js
+++ todo-react-app/src/Todo.js
@@ -13,6 +13,7 @@ class Todo extends React.Component {
     super(props);
     this.state = { item: props.item, readOnly: true };
     this.delete = props.delete;
+    this.update = props.update;
   }
 
   deleteEventHandler = () => {
@@ -23,6 +24,7 @@ class Todo extends React.Component {
     const thisItem = this.state.item;
     thisItem.done = !thisItem.done;
     this.setState({ item: thisItem });
+    this.update(this.state.item);
   };
 
   offReadOnlyMode = () => {
@@ -35,6 +37,7 @@ class Todo extends React.Component {
   enterKeyEventHandler = (e) => {
     if (e.key === "Enter") {
       this.setState({ readOnly: true });
+      this.update(this.state.item);
     }
   };
 
